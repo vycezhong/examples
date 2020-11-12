762232c194787bcd12965deb23af9dea8cd65f12
zhongyuchen <izhongyuchen@gmail.com>
2020-11-13 00:36:32 +0800

    test

--- imagenet/main.py
+++ imagenet/main.py
@@ -122,9 +122,9 @@ class Model(nn.Module):
 
     def __init__(self, num_classes: int = 1000) -> None:
         super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(3, 8, kernel_size=3, stride=1, padding=1)
         # self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc = nn.Linear(32*112*112, num_classes)
+        self.fc = nn.Linear(8*112*112, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.conv1(x)
