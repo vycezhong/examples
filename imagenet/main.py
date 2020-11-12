3a65930498055a1d0e3d1109a6bb8ea73e103697
zhongyuchen <izhongyuchen@gmail.com>
2020-11-13 00:35:10 +0800

    test

--- imagenet/main.py
+++ imagenet/main.py
@@ -123,13 +123,13 @@ class Model(nn.Module):
     def __init__(self, num_classes: int = 1000) -> None:
         super(Model, self).__init__()
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        # self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.fc = nn.Linear(32*112*112, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.conv1(x)
         x = F.relu(x, inplace=True)
-        x = self.pool(x)
+        # x = self.pool(x)
         x = torch.flatten(x, 1)
         x = self.fc(x)
         return x
