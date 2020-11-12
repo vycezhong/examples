227bb41a8c75853458aa85f47b09b23810922ec0
zhongyuchen <izhongyuchen@gmail.com>
2020-11-12 19:25:29 +0800

    update

--- imagenet/main.py
+++ imagenet/main.py
@@ -116,42 +116,14 @@ def main():
         main_worker(args.gpu, ngpus_per_node, args)
 
 
-class AlexNet(nn.Module):
+class Model(nn.Module):
 
     def __init__(self, num_classes: int = 1000) -> None:
-        super(AlexNet, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-        )
-        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
-        self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
-        )
+        super(Model, self).__init__()
+        self.linear = nn.Linear(3*224*224, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.features(x)
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.classifier(x)
-        return x
+        return self.linear(x.reshape(x.shape[0], -1))
 
 
 def main_worker(gpu, ngpus_per_node, args):
@@ -180,7 +152,7 @@ def main_worker(gpu, ngpus_per_node, args):
         # model = models.__dict__[args.arch]()
         pass
 
-    model = AlexNet()
+    model = Model()
 
     if not torch.cuda.is_available():
         print('using CPU, this will be slow')
