0d7eac59c24fff47ce4b2d8193b04ee63bf909c7
zhongyuchen <izhongyuchen@gmail.com>
2020-11-13 02:22:41 +0800

    test

--- imagenet/main.py
+++ imagenet/main.py
@@ -123,14 +123,29 @@ class Model(nn.Module):
     def __init__(self, num_classes: int = 1000) -> None:
         super(Model, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            # nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            # nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            # nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            # nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            # nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
         )
         self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
         self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(32*6*6, num_classes),
+            # nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            # nn.ReLU(inplace=True),
+            # nn.Dropout(),
+            nn.Linear(4096, 4096),
+            # nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
